5b2a7df6b3a9edfcad2a8744a6feb54a2a6cf363
kobekun <kobekun881105@126.com>
2019-06-18 01:46:26 +0800

    log日志测试

---
+++ src/test/java/LoggerGenerator.java
@@ -0,0 +1,21 @@
+import org.apache.log4j.Logger;
+
+/**
+ * 模拟日志产生
+ */
+public class LoggerGenerator {
+
+    public static Logger logger = Logger.getLogger(LoggerGenerator.class.getName());
+
+    public static void main(String[] args) throws Exception{
+
+        int index = 0;
+
+        while (true){
+
+            Thread.sleep(1000);
+
+            logger.info("value: " + index++);
+        }
+    }
+}
